a1c1f789e5b6c318df238e178f1ddc5d08953e6f
phucnguyen117 <144077716+phucnguyen117@users.noreply.github.com>
2023-12-19 22:12:00 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		int sum = 0;
+
+		while (i <= 100) {
+			sum += i;
+			i++;
+		}
+
+		System.out.println("Tổng các số từ 1 đến 100 là: " + sum);
+
+	}
+
+}
